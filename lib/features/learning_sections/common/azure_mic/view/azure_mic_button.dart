697b71e876cc729d95336cc495c6449951f9178f
ziadhassan7 <ziadhassan7@gmail.com>
2024-02-10 02:59:14 +0200

    UI tweaks for the mic

--- lib/features/learning_sections/common/azure_mic/view/azure_mic_button.dart
+++ lib/features/learning_sections/common/azure_mic/view/azure_mic_button.dart
@@ -2,13 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
 import 'package:pluperfect/core/azure_speech/azure_model.dart';
+import 'package:pluperfect/core/styles/color_theme.dart';
 import '../cubit/mic_cubit.dart';
 import '../cubit/mic_states.dart';
 
-class AzureMicButton extends StatelessWidget {
+class AzureMicButton extends StatefulWidget {
   const AzureMicButton({super.key, required this.onResponse});
 
   final Function(AzureModel) onResponse;
+
+  @override
+  State<AzureMicButton> createState() => _AzureMicButtonState();
+}
+
+class _AzureMicButtonState extends State<AzureMicButton> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +54,7 @@ class AzureMicButton extends StatelessWidget {
           if(state is IdleState){
             if(state.response != null){
               //trigger a score widget
-              onResponse(state.response!);
+              widget.onResponse(state.response!);
             }
             return idleButton();
           }
@@ -54,6 +76,9 @@ class AzureMicButton extends StatelessWidget {
     //Lottie.asset('assets/animations/blue.json'),
     //Lottie.asset('assets/animations/blue_good.json'),
     //Lottie.asset('assets/animations/cortona.json'),
+    //Lottie.asset('assets/animations/red_mic.json'),
+    //Lottie.asset('assets/animations/listening.json'),
+    //Lottie.asset('assets/animations/loading.json'),
     //Lottie.asset('assets/animations/mic.json'),
   }
 
@@ -62,6 +87,38 @@ class AzureMicButton extends StatelessWidget {
   }
 
   Widget button(String icon, bool shouldAnimate){
-    return Lottie.asset(icon, height: 120, width: 120, fit: BoxFit.fill, animate: shouldAnimate);
+    return Lottie.asset(
+        icon,
+        height: 120, width: 120, fit: BoxFit.fill,
+        animate: shouldAnimate,
+        frameRate: FrameRate.max,
+
+      delegates: LottieDelegates(
+        values: [
+          ValueDelegate.color(
+            const ['base', '**'],
+            value: ColorTheme.violet,
+          ),
+          ValueDelegate.color(
+            const ['Base Layer 4', '**'],
+            value: ColorTheme.violet,
+          ),
+          ValueDelegate.color(
+            const ['Base Layer 3', '**'],
+            value: ColorTheme.violet,
+          ),
+        ],
+      ),
+
+       /* controller: _controller,
+        onLoaded: (composition) {
+          // Configure the AnimationController
+          _controller
+            ..duration = composition.duration
+            ..forward();
+        },*/
+
+
+    );
   }
 }

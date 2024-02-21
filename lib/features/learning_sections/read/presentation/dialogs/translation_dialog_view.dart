436ea9e7586f69944f680fa8cd3d0f0d30b8648e
ziadhassan7 <ziadhassan7@gmail.com>
2024-02-21 02:25:06 +0200

    add dark mode -> to translation widget

--- lib/features/learning_sections/read/presentation/dialogs/translation_dialog_view.dart
+++ lib/features/learning_sections/read/presentation/dialogs/translation_dialog_view.dart
@@ -3,9 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pluperfect/core/app_widgets/text_view/text_view.dart';
 import 'package:pluperfect/core/constants/colors.dart';
 import 'package:pluperfect/core/gemini/gemini_client.dart';
-import 'package:pluperfect/core/openai/openai_client.dart';
-import 'package:pluperfect/core/styles/box_decoration.dart';
-import 'package:pluperfect/core/translate/translate_util.dart';
+import 'package:pluperfect/core/styles/color_theme.dart';
 import '../../../../../core/styles/padding.dart';
 import '../../../common/quotes_provider/quotes_controller.dart';
 
@@ -14,46 +12,49 @@ class TranslationDialogView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            decoration: BoxDecoration(
-                color: violetLight,
-                borderRadius: BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25))
-            ),
+    return Container(
+      color: ColorTheme.background,
+      child: Column(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  color: ColorTheme.violet,
+                  borderRadius: const BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25))
+              ),
 
-            padding: const CustomPadding(vertical: 14, horizontal: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const TextView("English", color: white,),
+              padding: const CustomPadding(vertical: 14, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const TextView("English", color: white,),
 
-                SvgPicture.asset("assets/arrow.svg"),
+                  SvgPicture.asset("assets/arrow.svg"),
 
-                const TextView("Arabic", color: white,),
-              ],
-            )),
+                  const TextView("Arabic", color: white,),
+                ],
+              )),
 
 
-        ///Word Translation
-        Padding(
-          padding: const CustomPadding.all(40),
-          child: FutureBuilder(
-            future: getTranslation(),
-            builder: (context, AsyncSnapshot<String?> snapshot) {
+          ///Word Translation
+          Padding(
+            padding: const CustomPadding.all(40),
+            child: FutureBuilder(
+              future: getTranslation(),
+              builder: (context, AsyncSnapshot<String?> snapshot) {
 
-              if(snapshot.hasData){
+                if(snapshot.hasData){
 
-                String? translated = snapshot.data;
-                return TextView(translated ?? "لا يوجد ترجمة", scale: TypeScale.arabic,);
+                  String? translated = snapshot.data;
+                  return TextView(translated ?? "لا يوجد ترجمة", scale: TypeScale.arabic,);
 
-              } else {
-                return const Center(child: CircularProgressIndicator(),);
+                } else {
+                  return const Center(child: CircularProgressIndicator(),);
+                }
               }
-            }
-          ),
-        )
-      ],
+            ),
+          )
+        ],
+      ),
     );
   }
 

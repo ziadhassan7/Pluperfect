3ce5a6e71cbc58819601a71632040f6387df1839
ziadhassan7 <ziadhassan7@gmail.com>
2024-03-01 15:10:48 +0200

    add sql local db support

---
+++ lib/core/sql/sql_client.dart
@@ -0,0 +1,42 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../features/dictionary/logic/local_db/dictionary_table_mangment.dart';
+
+
+class SqlClient {
+
+  static final SqlClient instance = SqlClient._init();
+
+  SqlClient._init();
+
+
+  static Database? _database;
+
+  Future<Database>? get database async{
+
+    if(_database != null) _database;
+
+    _database = await _initDB();
+    return _database!;
+  }
+
+  //initialize database
+  Future<Database> _initDB() async{
+    String path = join(await getDatabasesPath(), 'BookDatabase.db');
+    return await openDatabase(path, version: 20, onCreate: _createDB, onUpgrade: _onUpgrade);
+  }
+
+
+  //create new db
+  Future<void> _createDB(Database db, int version) async {
+    // Book Table
+    await DictionaryTable.create(db);
+  }
+
+  // This is called for users who have old db
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    // Book Table
+    await DictionaryTable.upgrade(db, oldVersion, newVersion);
+  }
+}

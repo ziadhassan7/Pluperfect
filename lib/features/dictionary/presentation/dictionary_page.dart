5383858d1c68b5bca821bb83665a3b71441f7b2d
ziadhassan7 <ziadhassan7@gmail.com>
2024-03-01 15:11:28 +0200

    add dictionary_page

---
+++ lib/features/dictionary/presentation/dictionary_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DictionaryPage extends StatelessWidget {
+  const DictionaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

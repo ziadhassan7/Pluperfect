a2d6299befb19304c575140412829504ad10db4f
ziadhassan7 <ziadhassan7@gmail.com>
2024-03-02 02:02:03 +0200

    Simplify widget

--- lib/features/learning_sections/common/pop_up_word_widget/translation_view.dart
+++ lib/features/learning_sections/common/pop_up_word_widget/translation_view.dart
@@ -1,38 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pluperfect/core/app_widgets/text_view/text_view.dart';
 import 'package:pluperfect/core/styles/padding.dart';
-import 'package:pluperfect/core/translate/translate_util.dart';
 
 class TranslationView extends StatelessWidget {
-  const TranslationView({super.key, required this.word});
+  const TranslationView({super.key, required this.translation});
 
-  final String word;
+  final String translation;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const CustomPadding(vertical: 12, horizontal: 12),
 
-      child: FutureBuilder(
-        future: getTranslation(word),
-        builder: (context, AsyncSnapshot<String> snapshot){
-
-          if(snapshot.hasData){
-            String translation = snapshot.data!;
-            return Align(
-                alignment: Alignment.topRight,
-                child: TextView(translation, scale: TypeScale.arabic,));
-
-          } else {
-            return const Center(child: CircularProgressIndicator(),);
-          }
-        }
-      ),
+      child: Align(
+          alignment: Alignment.topRight,
+          child: TextView(translation, scale: TypeScale.arabic,)),
     );
-  }
-
-
-  getTranslation(String word){
-    return TranslateUtil.translate(word);
   }
 }

f807140ccf6cf650c373def90c15765ceb7b2769
ziadhassan7 <ziadhassan7@gmail.com>
2024-04-09 22:04:11 +0200

    reformat the custom text widget

--- lib/core/app_widgets/text_view/text_view.dart
+++ lib/core/app_widgets/text_view/text_view.dart
@@ -4,8 +4,7 @@ import 'package:pluperfect/core/constants/colors.dart';
 import 'package:pluperfect/core/localization/localization.dart';
 import 'package:pluperfect/core/styles/color_theme.dart';
 
-
-enum TypeScale{
+enum TypeScale {
   small,
   caption,
   description,
@@ -33,53 +32,101 @@ class TextView extends StatelessWidget {
     this.direction,
   }) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
     return getText(context);
   }
 
-  Widget getText(BuildContext context){
-
+  Widget getText(BuildContext context) {
     switch (scale) {
-    //Small
+      //Small
       case TypeScale.small:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: weight, size: 12, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Caption
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            weight: weight,
+            size: 12,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Caption
       case TypeScale.caption:
-        return CustomText(text, color: color ?? grey, weight: weight, size: 14, fontFamily: FontFamily.sansation,  maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Description
+        return CustomText(text,
+            color: color ?? grey,
+            weight: weight,
+            size: 14,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Description
       case TypeScale.description:
-        return CustomText(text, color: color ?? ColorTheme.text, size: 16, fontFamily: FontFamily.sansation_light, maxLine: 1000,  textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //UserInput
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            size: 16,
+            fontFamily: FontFamily.sansation_light,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //UserInput
       case TypeScale.userInput:
-        return CustomText(text, color: grey, weight: weight, size: 16, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Normal
+        return CustomText(text,
+            color: grey,
+            weight: weight,
+            size: 16,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Normal
       case TypeScale.normal:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: weight, size: 16, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Headline1
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            weight: weight,
+            size: 16,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Headline1
       case TypeScale.headline1:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: weight, size: 18, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Headline2
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            weight: weight,
+            size: 18,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Headline2
       case TypeScale.headline2:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: weight, size: 22, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Headline3
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            weight: weight,
+            size: 22,
+            fontFamily: FontFamily.sansation,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Headline3
       case TypeScale.headline3:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: FontWeight.bold, size: 25, fontFamily: FontFamily.comforta, maxLine: 1000, textDirection: direction ?? getTextDirectionBasedOnLocal(context));
-    //Arabic
+        return CustomText(text,
+            color: color ?? ColorTheme.text,
+            weight: FontWeight.bold,
+            size: 25,
+            fontFamily: FontFamily.comforta,
+            maxLine: 1000,
+            textDirection: direction ?? getTextDirectionBasedOnLocal(context));
+      //Arabic
       case TypeScale.arabic:
-        return CustomText(text, color: color ?? ColorTheme.text, weight: weight, size: 18, fontFamily: FontFamily.sansation, maxLine: 1000, textDirection: TextDirection.rtl,);
+        return CustomText(
+          text,
+          color: color ?? ColorTheme.text,
+          weight: weight,
+          size: 18,
+          fontFamily: FontFamily.sansation,
+          maxLine: 1000,
+          textDirection: TextDirection.rtl,
+        );
     }
-
   }
 
-
-  getTextDirectionBasedOnLocal(BuildContext context){
-
-    if(LocalizationHandler.getCurrentLocal(context) == const Locale('ar')){
+  getTextDirectionBasedOnLocal(BuildContext context) {
+    if (LocalizationHandler.getCurrentLocal(context) == const Locale('ar')) {
       return TextDirection.rtl;
-
     } else {
       return TextDirection.ltr;
     }

366f8def1168bfb68b6a0e936fd594ae13f2570d
ziadhassan7 <ziadhassan7@gmail.com>
2024-04-11 02:46:26 +0200

    Organize code

--- lib/features/learning_sections/common/words_handler/correct_words_processor.dart
+++ lib/features/learning_sections/common/words_handler/correct_words_processor.dart
@@ -27,35 +27,55 @@ class CorrectWordsProcessor {
   static Words _getMatchingWords(List<Words> userInputWords, String targetWord, int targetIndex){
 
     try{
-      if ((targetIndex > 0) &&
-          (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex - 1].word!))) {
 
-        Words word = userInputWords[targetIndex - 1];
-        return Words(word: targetWord, accuracyScore: word.accuracyScore, errorType: word.errorType);
+      if (_theWordBeforeMatched(userInputWords, targetWord, targetIndex)) {
+
+        Words wordThatMatched = userInputWords[targetIndex - 1];
+        return Words(word: targetWord, accuracyScore: wordThatMatched.accuracyScore, errorType: wordThatMatched.errorType);
       }
 
-      if ((targetIndex < userInputWords.length) &&
-          (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex].word!))) {
+      if (_theWordMatched(userInputWords, targetWord, targetIndex)) {
 
-        Words word = userInputWords[targetIndex];
-        return Words(word: targetWord, accuracyScore: word.accuracyScore, errorType: word.errorType);
+        Words wordThatMatched = userInputWords[targetIndex];
+        return Words(word: targetWord, accuracyScore: wordThatMatched.accuracyScore, errorType: wordThatMatched.errorType);
       }
 
-      if ((targetIndex < userInputWords.length - 1) &&
-          (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex + 1].word!))) {
+      if (_theWordAfterMatched(userInputWords, targetWord, targetIndex)) {
 
-        Words word = userInputWords[targetIndex + 1];
-        return Words(word: targetWord, accuracyScore: word.accuracyScore, errorType: word.errorType);
+        Words wordThatMatched = userInputWords[targetIndex + 1];
+        return Words(word: targetWord, accuracyScore: wordThatMatched.accuracyScore, errorType: wordThatMatched.errorType);
       }
+
     } catch (e){
       return Words(word: targetWord, accuracyScore: 0.0, errorType: "");
     }
-
     return Words(word: targetWord, accuracyScore: 0.0, errorType: "");
   }
 
 
 
+  static bool _theWordBeforeMatched(List<Words> userInputWords, String targetWord, int targetIndex,){
 
+    bool isTargetWordNotFirstWord = (targetIndex > 0); //cuz first word doesn't have a word before
+    bool wordBeforeMatched = (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex - 1].word!));
+
+    return isTargetWordNotFirstWord && wordBeforeMatched;
+  }
+
+  static bool _theWordMatched(List<Words> userInputWords, String targetWord, int targetIndex,){
+
+    bool inBoundsOfArray = (targetIndex < userInputWords.length);
+    bool wordMatched = (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex].word!));
+
+    return inBoundsOfArray && wordMatched;
+  }
+
+  static bool _theWordAfterMatched(List<Words> userInputWords, String targetWord, int targetIndex,){
+
+    bool isTargetWordNotLastWord = (targetIndex < userInputWords.length - 1);
+    bool wordAfterMatched = (WordsComparison.compareIgnoreCase(targetWord, userInputWords[targetIndex + 1].word!));
+
+    return isTargetWordNotLastWord && wordAfterMatched;
+  }
 
 }

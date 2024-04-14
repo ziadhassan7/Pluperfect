4227a2a61b2027292c1996d201b22a08a85f5562
ziadhassan7 <ziadhassan7@gmail.com>
2024-04-14 11:03:49 +0200

    Detect gemini exceptions

--- lib/core/gemini/gemini_chat_client.dart
+++ lib/core/gemini/gemini_chat_client.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_gemini/src/models/candidates/candidates.dart';
+import '../custom_log.dart';
 
 
 class GeminiChatClient {
@@ -8,39 +9,53 @@ class GeminiChatClient {
   static List<Content> content = [];
 
 
-
   static Future<String?> openChatRequest(String initialMessage){
-    content = [];
-    return postChatRequest(initialMessage);
+    try{
+      content = [];
+      return postChatRequest(initialMessage);
+
+    } catch (e){
+      Log.p("Gemini - openChatRequest: $e");
+      rethrow;
+    }
   }
 
 
   /// Chat
   static Future<String?> postChatRequest(String input) async {
+    try {
+      // Add user input to the chat list
+      content.add(
+        Content(parts: [Parts(text: input)], role: 'user'),
+      );
 
-    // Add user input to the chat list
-    content.add(
-      Content(parts: [
-        Parts(text: input)],
-          role: 'user'),
-    );
+      // Get reply
+      Candidates? candidate = await _getChatAnswer(content);
 
-    // Get reply
-    Candidates? candidate = await _getChatAnswer(content);
-
-    // Add AI input to the chat list
-    if(candidate != null){
-      if(candidate.content != null){
-        content.add(candidate.content!);
+      // Add AI input to the chat list
+      if (candidate != null) {
+        if (candidate.content != null) {
+          content.add(candidate.content!);
+        }
       }
-    }
 
-    return candidate?.output; // return text answer
+      return candidate?.output; // return text answer
+
+    } catch (e){
+      Log.p("Gemini - postChatRequest: $e");
+      rethrow;
+    }
   }
 
 
   static Future<Candidates?> _getChatAnswer(List<Content> input){
-    return _gemini.chat(input);
+    try {
+      return _gemini.chat(input);
+
+    } catch (e){
+      Log.p("Gemini - getChatAnswer: $e");
+      rethrow;
+    }
   }
 
 }
